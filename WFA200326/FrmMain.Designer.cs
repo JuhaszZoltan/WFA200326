86efb28b02ff29ca602dbe18e2584245b97c24e5
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-03-26 23:16:24 +0100

    Add project files.

---
+++ WFA200326/FrmMain.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace WFA200326
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbPohar = new System.Windows.Forms.CheckBox();
+            this.cbCukor = new System.Windows.Forms.CheckBox();
+            this.cbTej = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnKeszit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.rbTea = new System.Windows.Forms.RadioButton();
+            this.rbCsoki = new System.Windows.Forms.RadioButton();
+            this.rbKv = new System.Windows.Forms.RadioButton();
+            this.lblAr = new System.Windows.Forms.Label();
+            this.lblEgs = new System.Windows.Forms.Label();
+            this.pbKeszul = new System.Windows.Forms.ProgressBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbPohar);
+            this.groupBox1.Controls.Add(this.cbCukor);
+            this.groupBox1.Controls.Add(this.cbTej);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Snow;
+            this.groupBox1.Location = new System.Drawing.Point(39, 255);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(392, 182);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Extra";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.SaddleBrown;
+            this.label5.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.Location = new System.Drawing.Point(261, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "- 10 Ft";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.SaddleBrown;
+            this.label3.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(253, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "+ 20 Ft";
+            // 
+            // cbPohar
+            // 
+            this.cbPohar.AutoSize = true;
+            this.cbPohar.BackColor = System.Drawing.Color.SaddleBrown;
+            this.cbPohar.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cbPohar.Location = new System.Drawing.Point(28, 123);
+            this.cbPohar.Name = "cbPohar";
+            this.cbPohar.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.cbPohar.Size = new System.Drawing.Size(187, 36);
+            this.cbPohar.TabIndex = 2;
+            this.cbPohar.Text = "Saját Pohár";
+            this.cbPohar.UseVisualStyleBackColor = false;
+            // 
+            // cbCukor
+            // 
+            this.cbCukor.AutoSize = true;
+            this.cbCukor.BackColor = System.Drawing.Color.SaddleBrown;
+            this.cbCukor.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cbCukor.Location = new System.Drawing.Point(28, 82);
+            this.cbCukor.Name = "cbCukor";
+            this.cbCukor.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.cbCukor.Size = new System.Drawing.Size(138, 36);
+            this.cbCukor.TabIndex = 2;
+            this.cbCukor.Text = "+ Cukor";
+            this.cbCukor.UseVisualStyleBackColor = false;
+            // 
+            // cbTej
+            // 
+            this.cbTej.AutoSize = true;
+            this.cbTej.BackColor = System.Drawing.Color.SaddleBrown;
+            this.cbTej.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cbTej.Location = new System.Drawing.Point(28, 39);
+            this.cbTej.Name = "cbTej";
+            this.cbTej.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.cbTej.Size = new System.Drawing.Size(97, 36);
+            this.cbTej.TabIndex = 2;
+            this.cbTej.Text = "+ Tej";
+            this.cbTej.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.SaddleBrown;
+            this.label2.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(263, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "+ 10 Ft";
+            // 
+            // btnKeszit
+            // 
+            this.btnKeszit.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnKeszit.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnKeszit.ForeColor = System.Drawing.Color.Snow;
+            this.btnKeszit.Location = new System.Drawing.Point(39, 560);
+            this.btnKeszit.Name = "btnKeszit";
+            this.btnKeszit.Size = new System.Drawing.Size(392, 80);
+            this.btnKeszit.TabIndex = 3;
+            this.btnKeszit.Text = "Elkészít!";
+            this.btnKeszit.UseVisualStyleBackColor = false;
+            this.btnKeszit.Click += new System.EventHandler(this.BtnKeszit_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Poiret One", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(39, 474);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 53);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ár:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.rbTea);
+            this.groupBox2.Controls.Add(this.rbCsoki);
+            this.groupBox2.Controls.Add(this.rbKv);
+            this.groupBox2.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox2.ForeColor = System.Drawing.Color.Snow;
+            this.groupBox2.Location = new System.Drawing.Point(39, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(392, 182);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ital";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.SaddleBrown;
+            this.label4.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(275, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 32);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "80 Ft";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.SaddleBrown;
+            this.label6.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label6.Location = new System.Drawing.Point(265, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 32);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "120 Ft";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.SaddleBrown;
+            this.label7.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label7.Location = new System.Drawing.Point(259, 40);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 32);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "100 Ft";
+            // 
+            // rbTea
+            // 
+            this.rbTea.AutoSize = true;
+            this.rbTea.BackColor = System.Drawing.Color.SaddleBrown;
+            this.rbTea.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rbTea.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.rbTea.Location = new System.Drawing.Point(28, 122);
+            this.rbTea.Name = "rbTea";
+            this.rbTea.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.rbTea.Size = new System.Drawing.Size(86, 36);
+            this.rbTea.TabIndex = 1;
+            this.rbTea.Text = "Tea";
+            this.rbTea.UseVisualStyleBackColor = false;
+            // 
+            // rbCsoki
+            // 
+            this.rbCsoki.AutoSize = true;
+            this.rbCsoki.BackColor = System.Drawing.Color.SaddleBrown;
+            this.rbCsoki.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rbCsoki.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.rbCsoki.Location = new System.Drawing.Point(28, 80);
+            this.rbCsoki.Name = "rbCsoki";
+            this.rbCsoki.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.rbCsoki.Size = new System.Drawing.Size(182, 36);
+            this.rbCsoki.TabIndex = 1;
+            this.rbCsoki.Text = "Forró Csoki";
+            this.rbCsoki.UseVisualStyleBackColor = false;
+            // 
+            // rbKv
+            // 
+            this.rbKv.AutoSize = true;
+            this.rbKv.BackColor = System.Drawing.Color.SaddleBrown;
+            this.rbKv.Font = new System.Drawing.Font("Poiret One", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rbKv.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.rbKv.Location = new System.Drawing.Point(28, 38);
+            this.rbKv.Name = "rbKv";
+            this.rbKv.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.rbKv.Size = new System.Drawing.Size(108, 36);
+            this.rbKv.TabIndex = 1;
+            this.rbKv.Text = "Kávé";
+            this.rbKv.UseVisualStyleBackColor = false;
+            // 
+            // lblAr
+            // 
+            this.lblAr.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.lblAr.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAr.Font = new System.Drawing.Font("Poiret One", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblAr.ForeColor = System.Drawing.Color.Snow;
+            this.lblAr.Location = new System.Drawing.Point(192, 474);
+            this.lblAr.Name = "lblAr";
+            this.lblAr.Size = new System.Drawing.Size(239, 53);
+            this.lblAr.TabIndex = 4;
+            this.lblAr.Text = "### Ft";
+            this.lblAr.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblEgs
+            // 
+            this.lblEgs.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.lblEgs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblEgs.Font = new System.Drawing.Font("Poiret One", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblEgs.ForeColor = System.Drawing.Color.Snow;
+            this.lblEgs.Location = new System.Drawing.Point(39, 721);
+            this.lblEgs.Name = "lblEgs";
+            this.lblEgs.Size = new System.Drawing.Size(392, 53);
+            this.lblEgs.TabIndex = 4;
+            this.lblEgs.Text = "Egészségedre!";
+            this.lblEgs.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblEgs.Visible = false;
+            // 
+            // pbKeszul
+            // 
+            this.pbKeszul.BackColor = System.Drawing.Color.Goldenrod;
+            this.pbKeszul.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.pbKeszul.Location = new System.Drawing.Point(39, 662);
+            this.pbKeszul.Name = "pbKeszul";
+            this.pbKeszul.Size = new System.Drawing.Size(392, 31);
+            this.pbKeszul.Step = 5;
+            this.pbKeszul.TabIndex = 5;
+            this.pbKeszul.Visible = false;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WFA200326.Properties.Resources.bg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(467, 793);
+            this.Controls.Add(this.pbKeszul);
+            this.Controls.Add(this.lblEgs);
+            this.Controls.Add(this.lblAr);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKeszit);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "Kávéautomata :3";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox cbTej;
+        private System.Windows.Forms.Button btnKeszit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbPohar;
+        private System.Windows.Forms.CheckBox cbCukor;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton rbTea;
+        private System.Windows.Forms.RadioButton rbCsoki;
+        private System.Windows.Forms.RadioButton rbKv;
+        private System.Windows.Forms.Label lblAr;
+        private System.Windows.Forms.Label lblEgs;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ProgressBar pbKeszul;
+    }
+}
+
---
+++ WFA200326/FrmMain.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA200326
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            this.Icon = Properties.Resources.cfIcon;
+
+            rbKv.CheckedChanged += RbsNCbs_CheckedChanged;
+            rbCsoki.CheckedChanged += RbsNCbs_CheckedChanged;
+            rbTea.CheckedChanged += RbsNCbs_CheckedChanged;
+            cbCukor.CheckedChanged += RbsNCbs_CheckedChanged;
+            cbTej.CheckedChanged += RbsNCbs_CheckedChanged;
+            cbPohar.CheckedChanged += RbsNCbs_CheckedChanged;
+
+            rbKv.Checked = true;
+        }
+
+        private void RbsNCbs_CheckedChanged(object sender, EventArgs e)
+        {
+            int ar = 0;
+
+            cbTej.Enabled = true;
+
+            if (rbKv.Checked) ar = 100;
+            else if (rbCsoki.Checked) ar = 120;
+            else if (rbTea.Checked)
+            {
+                ar = 80;
+                cbTej.Checked = false;
+                cbTej.Enabled = false;
+            }
+
+            if (cbTej.Checked) ar += 10;
+            if (cbCukor.Checked) ar += 20;
+            if (cbPohar.Checked) ar -= 10;
+
+            lblAr.Text = $"{ar} Ft";
+        }
+
+        private void BtnKeszit_Click(object sender, EventArgs e)
+        {
+            pbKeszul.Visible = true;
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if(pbKeszul.Value == 100)
+            {
+                timer.Stop();
+                lblEgs.Visible = true;
+            }
+            else pbKeszul.Value += 20;
+        }
+    }
+}
